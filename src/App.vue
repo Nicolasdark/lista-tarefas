84c0b47ee7c3bfbd0620bc07fbb1d2415a8ccbaa
Nicolas Silva <nicolasandreislc@gmail.com>
2021-09-20 21:45:42 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,60 @@
 <template>
- <div class="container">
-   <h2>{{ titulo }}</h2>
-   <div class="input-group">
-     <input @keyup.enter="adicionarTarefa" v-model="novaTarefa" type="text">
-     <span class="input-group-btn">
-     <button @click="adicionartarefa" class="btn btn-primary">adicionar</button>
-     </span>
-   </div>
-   <ul>
-     <li v-for="(tarefa, index) in tarefas" :key="index" :class="{ removed: tarefa.checked }">
-       <input type="checkbox" v-model="tarefa.checked">
-       <label>{{tarefa.titulo}}</label>
-     </li>
-   </ul>
-   <footer>
-     <em>altere aqui o titulo da sua lista de tarefas</em>
-     <input v-model="titulo" type="text"/>
+  <section>
+  <cabecalho />
+  <div class="container">
+    <h2>{{ titulo }}</h2>
+    <div class="input-group">
+      <input @keyup.enter="adicionarTarefa" v-model="novaTarefa" type="text" />
+      <span class="input-group-btn">
+        <button @click="adicionarTarefa" class="btn btn-primary">
+          Adicionar tarefa
+        </button>
+      </span>
+    </div>
+    <ul>
+      <li
+        v-for="(tarefa, index) in tarefas"
+        :key="index"
+        :class="{ removed: tarefa.checked }"
+      >
+        <input type="checkbox" v-model="tarefa.checked" />
+        <label>{{ tarefa.titulo }}</label>
+      </li>
+    </ul>
+    <footer>
+      <em>Altere aqui o título da sua lista de tarefas</em>
+      <input v-model="titulo" type="text" />
     </footer>
   </div>
+  <rodape />
+</section>  
 </template>
 
 <script>
-
+import cabecalho from "@/components/cabecalho"
+import rodape from "@/components/rodape"
 export default {
-  name: 'App',
+  name: "App",
+  components: { cabecalho, rodape },
   data() {
-    return{
+    return {
       titulo: "Minha lista de tarefas",
-      novaTarefa: " ",
+      novaTarefa: "",
       tarefas: [
-        { titulo: "estudar", checked: false},
-        { titulo: "programar", checked: true},
+        { titulo: "Estudar", checked: false },
+        { titulo: "Programar", checked: false },
       ],
     };
   },
   methods: {
     adicionarTarefa() {
       this.tarefas.push({
-        titulo:this.novaTarefa,
-        checked: false
-      })
+        titulo: this.novaTarefa,
+        checked: false,
+      });
       this.novaTarefa = "";
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -51,15 +63,12 @@ export default {
   width: 40%;
   margin: 20px auto 0px auto;
 }
-
 ul li {
-    list-style:none;
-  }
-
+  list-style: none;
+}
 .removed {
   color: gray;
 }
-
 .removed label {
   text-decoration: line-through;
 }
---
+++ src/components/cabecalho.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+      <h1>Minha lista de tarefas</h1>
+  </header>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+header {
+  background-color: royalblue;
+  color: white;
+  height: 50px;
+  width: 100%;
+  display: flex;
+}
+
+</style>
